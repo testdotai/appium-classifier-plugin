505d89040dddebf7109a636914a86ffe8ad32674
Jonathan Lipps <jlipps@gmail.com>
2018-10-12 11:50:13 -0700

    factor out test and prefer accessibility id

--- test/find-e2e-specs.js
+++ test/find-e2e-specs.js
@@ -2,7 +2,6 @@ import wd from 'wd';
 import path from 'path';
 import chai from 'chai';
 import should from 'should';
-import B from 'bluebird';
 
 chai.use(should);
 
@@ -17,6 +16,14 @@ const WALMART = {
   appWaitActivity: '.app.main.HomeActivity',
 };
 
+const ANDROID = {
+  platformName: 'Android',
+  deviceName: 'Android Emulator',
+  automationName: 'UiAutomator2',
+  customFindModules: {ai: MODULE_PATH},
+  shouldUseCompactResponses: false,
+};
+
 describe('Finding', function () {
   let driver;
 
@@ -25,15 +32,8 @@ describe('Finding', function () {
     // appium server should be running on below host/port, with this
     // test-ai-classifier package `npm link`ed into it
     driver = wd.promiseChainRemote('http://localhost:4723/wd/hub');
-    await driver.init({
-      ...WALMART,
-      platformName: 'Android',
-      deviceName: 'Android Emulator',
-      automationName: 'UiAutomator2',
-      customFindModules: {ai: MODULE_PATH},
-      shouldUseCompactResponses: false,
-    });
-    await B.delay(8000); // wait just a bit for things to be ready
+    await driver.init({...WALMART, ...ANDROID});
+    await driver.setImplicitWaitTimeout(20000);
   });
 
   after(async function () {
@@ -44,8 +44,8 @@ describe('Finding', function () {
 
   it('should find an element by its label', async function () {
     this.timeout(60000);
+    await driver.elementByAccessibilityId('Open navigation drawer');
     await driver.elementByCustom('ai:cart').click();
-    await B.delay(2000);
-    await driver.elementByXPath('//*[@text="SIGN IN TO SEE"]');
+    await driver.elementByAccessibilityId('Estimated Tax');
   });
 });
