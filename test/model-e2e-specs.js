7f77ba22e85bd27e324632ff868a4d31bc8a4e9e
Jonathan Lipps <jlipps@gmail.com>
2019-10-22 21:04:52 -0700

    update deps and clean up model tests so all tests pass

--- test/model-e2e-specs.js
+++ test/model-e2e-specs.js
@@ -1,5 +1,4 @@
 import path from 'path';
-import fs from 'fs';
 import chai from 'chai';
 import should from 'should';
 import { asyncmap } from 'asyncbox';
@@ -20,25 +19,6 @@ const TINY_MENU_IMG = path.resolve(FIXTURES, "menu_small.png");
 describe('Model', function () {
   it('should load the model', async function () {
     await getModel();
-  });
-
-  it('should get the tensorflow version', function () {
-    TF_VERSION.should.match(/^1\./);
-  });
-
-  it('should load a model natively - birds', function () {
-    const res = detect("/Users/jlipps/Desktop/objDetection/saved_model", "/Users/jlipps/Desktop/birds.jpg", 0.95, true);
-    res.length.should.eql(2);
-    should.exist(res[0].confidence);
-    res[0].ymin.should.be.above(0.5);
-    res[0].ymin.should.be.below(0.6);
-    res[1].ymin.should.be.above(0.1);
-    res[1].ymin.should.be.below(0.2);
-  });
-
-  it('should load a model natively - mobile', function () {
-    const res = detect("/Users/jlipps/Desktop/objDetection/saved_model", "/Users/jlipps/Desktop/screen.jpg", 0.95, true);
-    console.log(res);
   });
 
   it.skip('should load and save a tensor', async function () {
