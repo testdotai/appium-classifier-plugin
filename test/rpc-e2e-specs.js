c03bfe2e03be61219fbb11578d424b7401886b9d
Jonathan Lipps <jlipps@gmail.com>
2019-11-20 16:30:30 -0800

    add additional test of rpc classifier

--- test/rpc-e2e-specs.js
+++ test/rpc-e2e-specs.js
@@ -10,7 +10,7 @@ chai.use(should);
 const PORT = 50051;
 const FIXTURES = path.resolve(__dirname, "..", "..", "test", "fixtures");
 const CART_IMG = path.resolve(FIXTURES, "cart.png");
-//const MIC_IMG = path.resolve(FIXTURES, "microphone.png");
+const MIC_IMG = path.resolve(FIXTURES, "microphone.png");
 //const FOLDER_IMG = path.resolve(FIXTURES, "folder.png");
 //const MENU_IMG = path.resolve(FIXTURES, "menu.png");
 //const TINY_MENU_IMG = path.resolve(FIXTURES, "menu_small.png");
@@ -28,18 +28,26 @@ describe('RPC server', function () {
 
   it('should handle requests to classify elements by image', async function () {
     const c = new ClassifierClient({port: PORT});
-    const elId = '1234-134';
     const input = {
       labelHint: 'cart',
       elementImages: {
-        [elId]: fs.readFileSync(CART_IMG)
+        cart: fs.readFileSync(CART_IMG),
+        mic: fs.readFileSync(MIC_IMG)
       },
+      confidenceThreshold: 0.0,
+      allowWeakerMatches: true
     };
     const res = await c.classifyElements(input);
-    res[elId].label.should.eql('cart');
-    res[elId].confidence.should.be.above(0.9);
-    res[elId].confidence.should.be.below(1.0);
-    res[elId].confidenceForHint.should.be.above(0.9);
-    res[elId].confidenceForHint.should.be.below(1.0);
+    res.cart.label.should.eql('cart');
+    res.cart.confidence.should.be.above(0.9);
+    res.cart.confidence.should.be.below(1.0);
+    res.cart.confidenceForHint.should.be.above(0.9);
+    res.cart.confidenceForHint.should.be.below(1.0);
+
+    res.mic.label.should.eql('microphone');
+    res.mic.confidence.should.be.above(0.2);
+    res.mic.confidence.should.be.below(1.0);
+    res.mic.confidenceForHint.should.be.above(0.0);
+    res.mic.confidenceForHint.should.be.below(0.2);
   });
 });
